22abfa54c1bd164464f86125b3f3290e52fcad41
maulanaa121 <fm886211@gmail.com>
2024-04-21 12:35:29 +0700

    first init github

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuIcon = document.getElementById('menu-icon');
+const navMid = document.getElementById('nav-mid');
+
+menuIcon.addEventListener('click',()=>{
+    navMid.classList.toggle('show')
+})
+
+
+
